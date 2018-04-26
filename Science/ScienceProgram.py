ede486a1ee0aceb990d5733ee058e47bb7a0a1af
Collin Rumpca <collinrumpca@gmail.com>
2018-04-26 15:06:26 -0500

    Add files via upload

---
+++ Science/ScienceProgram.py
@@ -0,0 +1,78 @@
+import requests
+from scipy import misc
+from io import BytesIO
+import json
+import sqlite3
+import datetime
+#from matplotlib import pyplot as plt
+#import numpy as np
+conn = sqlite3.connect('weather.db')
+db = conn.cursor()
+key ="0f955463db11c0c103e6d6ebfa5fde40"
+#          https://api.darksky.net/forecast/[key]/[latitude],[longitude],[time]
+for time in range (0, 1523768400, 86400):
+    URL = "https://api.darksky.net/forecast/" + key + "/44.071071, -103.217924," + str(time) +"?exclude=hourly,currently,minutely,alerts,flags"
+    try:
+        r = requests.get(url=URL)
+        strDate = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d')
+        data = r.json()
+        #(day varchar[14], temperatureMin float, temperatureMax float, precipProbability float, precipAccumulation float, precipType text, pressure float, humidity float, windSpeed float, visibility float, cloudCover float);
+        executeString = "INSERT INTO rapidCitySD (day,temperatureMin,temperatureMax,precipProbability,pressure,humidity,windSpeed,visibility) values(\""+strDate +"\","+ str(data["daily"]["data"][0]["temperatureMin"])+","+ str(data["daily"]["data"][0]["temperatureMax"]) +","+ str(data["daily"]["data"][0]["precipProbability"]) +","+ str(data["daily"]["data"][0]["pressure"]) +","+ str(data["daily"]["data"][0]["humidity"]) +","+ str(data["daily"]["data"][0]["windSpeed"]) +","+ str(data["daily"]["data"][0]["visibility"]) +")"
+        print(executeString)
+        db.execute(executeString)
+        conn.commit()
+    except:
+        print("url: ", URL ,"does not exist")
+for time in range (0, 1523768400, 86400):
+    URL = "https://api.darksky.net/forecast/" + key + "/44.375163, -100.319996," + str(time) +"?exclude=hourly,currently,minutely,alerts,flags"
+    try:
+        r = requests.get(url=URL)
+        strDate = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d')
+        data = r.json()
+        #(day varchar[14], temperatureMin float, temperatureMax float, precipProbability float, precipAccumulation float, precipType text, pressure float, humidity float, windSpeed float, visibility float, cloudCover float);
+        executeString = "INSERT INTO pierreSD (day,temperatureMin,temperatureMax,precipProbability,pressure,humidity,windSpeed,visibility) values(\""+strDate +"\","+ str(data["daily"]["data"][0]["temperatureMin"])+","+ str(data["daily"]["data"][0]["temperatureMax"]) +","+ str(data["daily"]["data"][0]["precipProbability"]) +","+ str(data["daily"]["data"][0]["pressure"]) +","+ str(data["daily"]["data"][0]["humidity"]) +","+ str(data["daily"]["data"][0]["windSpeed"]) +","+ str(data["daily"]["data"][0]["visibility"]) +")"
+        print(executeString)
+        db.execute(executeString)
+        conn.commit()
+    except:
+        print("url: ", URL ,"does not exist")
+for time in range (0, 1523768400, 86400):
+    URL = "https://api.darksky.net/forecast/" + key + "/44.375163, -100.319996," + str(time) +"?exclude=hourly,currently,minutely,alerts,flags"
+    try:
+        r = requests.get(url=URL)
+        strDate = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d')
+        data = r.json()
+        #(day varchar[14], temperatureMin float, temperatureMax float, precipProbability float, precipAccumulation float, precipType text, pressure float, humidity float, windSpeed float, visibility float, cloudCover float);
+        executeString = "INSERT INTO pierreSD (day,temperatureMin,temperatureMax,precipProbability,pressure,humidity,windSpeed,visibility) values(\""+strDate +"\","+ str(data["daily"]["data"][0]["temperatureMin"])+","+ str(data["daily"]["data"][0]["temperatureMax"]) +","+ str(data["daily"]["data"][0]["precipProbability"]) +","+ str(data["daily"]["data"][0]["pressure"]) +","+ str(data["daily"]["data"][0]["humidity"]) +","+ str(data["daily"]["data"][0]["windSpeed"]) +","+ str(data["daily"]["data"][0]["visibility"]) +")"
+        print(executeString)
+        db.execute(executeString)
+        conn.commit()
+    except:
+        print("url: ", URL ,"does not exist")
+for time in range (0, 1523768400, 86400):
+    URL = "https://api.darksky.net/forecast/" + key + "/44.302416, -96.785336," + str(time) +"?exclude=hourly,currently,minutely,alerts,flags"
+    try:
+        r = requests.get(url=URL)
+        strDate = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d')
+        data = r.json()
+        #(day varchar[14], temperatureMin float, temperatureMax float, precipProbability float, precipAccumulation float, precipType text, pressure float, humidity float, windSpeed float, visibility float, cloudCover float);
+        executeString = "INSERT INTO brookingsSD (day,temperatureMin,temperatureMax,precipProbability,pressure,humidity,windSpeed,visibility) values(\""+strDate +"\","+ str(data["daily"]["data"][0]["temperatureMin"])+","+ str(data["daily"]["data"][0]["temperatureMax"]) +","+ str(data["daily"]["data"][0]["precipProbability"]) +","+ str(data["daily"]["data"][0]["pressure"]) +","+ str(data["daily"]["data"][0]["humidity"]) +","+ str(data["daily"]["data"][0]["windSpeed"]) +","+ str(data["daily"]["data"][0]["visibility"]) +")"
+        print(executeString)
+        db.execute(executeString)
+        conn.commit()
+    except:
+        print("url: ", URL ,"does not exist")
+for time in range (0, 1523768400, 86400):
+    URL = "https://api.darksky.net/forecast/" + key + "/44.8994, -97.1151," + str(time) +"?exclude=hourly,currently,minutely,alerts,flags"
+    try:
+        r = requests.get(url=URL)
+        strDate = datetime.datetime.fromtimestamp(time).strftime('%Y-%m-%d')
+        data = r.json()
+        #(day varchar[14], temperatureMin float, temperatureMax float, precipProbability float, precipAccumulation float, precipType text, pressure float, humidity float, windSpeed float, visibility float, cloudCover float);
+        executeString = "INSERT INTO watertownSD (day,temperatureMin,temperatureMax,precipProbability,pressure,humidity,windSpeed,visibility) values(\""+strDate +"\","+ str(data["daily"]["data"][0]["temperatureMin"])+","+ str(data["daily"]["data"][0]["temperatureMax"]) +","+ str(data["daily"]["data"][0]["precipProbability"]) +","+ str(data["daily"]["data"][0]["pressure"]) +","+ str(data["daily"]["data"][0]["humidity"]) +","+ str(data["daily"]["data"][0]["windSpeed"]) +","+ str(data["daily"]["data"][0]["visibility"]) +")"
+        print(executeString)
+        db.execute(executeString)
+        conn.commit()
+    except:
+        print("url: ", URL ,"does not exist")
+conn.close()
---
+++ Science/Spreadsheet.py
@@ -0,0 +1,34 @@
+import sqlite3
+import pyoo
+
+desktop = pyoo.Desktop('localhost', 2002)
+
+doc = desktop.create_spreadsheet()
+doc.sheets[0]
+sheet = doc.sheets[0]
+
+sheet[0,0].value = 0
+sheet[0,1].value = "Date"
+sheet[0,2].value = "Lowest Temp"
+sheet[0,3].value = "Highest Temp"
+sheet[0,4].value = "PrecipProbability"
+sheet[0,5].value = "Pressure"
+sheet[0,6].value = "Humidity"
+sheet[0,7].value = "Windspeed"
+sheet[0,8].value = "Visibility"
+
+y = 1
+conn = sqlite3.connect('weather.db')
+db = conn.cursor()
+
+for row in db.execute("select * from brookingsSD"):
+    sheet[y,0].value = y
+    sheet[y,1].value = row[0]
+    sheet[y,2].value = row[1]
+    sheet[y,3].value = row[2]
+    sheet[y,4].value = row[3]
+    sheet[y,5].value = row[4]
+    sheet[y,6].value = row[5]
+    sheet[y,7].value = row[6]
+    sheet[y,8].value = row[7]
+    y +=  1
